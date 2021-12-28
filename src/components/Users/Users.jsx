534b8f1ee96d1c49eb25b49cb6f22ab3e5c1acd7
unknown <gradov78@gmail.com>
2021-12-28 12:04:50 +0300

    54 lvl complete!

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,58 @@
+import React from 'react'
 import * as axios from 'axios'
 import css from './Users.module.css'
-import userPhoto from '../../asets/images/avatar3.jpg'
+import userPhoto from '../../assets/images/avatar4.jpg'
 
-const Users = (props) => {
-    console.log(props.users.length)
-    if (props.users.length === 0) {
+class Users extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    componentDidMount() {
         axios
             .get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                props.setUsersList(response.data.items)
+                this.props.setUsersList(response.data.items)
             })
     }
-
-    return (
-        <div className={css.usersInner}>
-            {
-                props.users.map(el => <div key={el.id} className={css.user}>
-                    <div className={css.avaAndButton}>
-                        <div>
-                            <img src={el.photos.small != null ? el.photos.small : userPhoto} />
+    render() {
+        return (
+            <div className={css.usersInner}>
+                {
+                    this.props.users.map(el => <div key={el.id} className={css.user}>
+                        <div className={css.avaAndButton}>
+                            <div>
+                                <img src={el.photos.small != null ? el.photos.small : userPhoto} />
+                            </div>
+                            <div>
+                                {el.followed
+                                    ? <button onClick={() => { this.props.unfollow(el.id) }}>Отписаться</button>
+                                    : <button onClick={() => { this.props.follow(el.id) }}>Подписаться</button>}
+                            </div>
                         </div>
-                        <div>
-                            {el.followed
-                                ? <button onClick={() => { props.unfollow(el.id) }}>Отписаться</button>
-                                : <button onClick={() => { props.follow(el.id) }}>Подписаться</button>}
+                        <div className={css.main}>
+                            <div>{el.name}</div>
+                            <p>{el.status}</p>
                         </div>
-                    </div>
-                    <div className={css.main}>
-                        <div>{el.name}</div>
-                        <p>{el.status}</p>
-                    </div>
-                    <div className={css.location}>
-                        <div>{'el.location.city'}</div>
-                        <div>{'el.location.country'}</div>
-                    </div>
-                </div>)}
-        </div>
-    )
+                        <div className={css.location}>
+                            <div>{'el.location.city'}</div>
+                            <div>{'el.location.country'}</div>
+                        </div>
+                    </div>)}
+                {/* <button className={css.getUsersButton} onClick={this.getUsers}>Загрузить пользователей</button> */}
+            </div>
+        )
+    }
+    // getUsers = () => {
+    //     if (this.props.users.length === 0) {
+    //         axios
+    //             .get("https://social-network.samuraijs.com/api/1.0/users")
+    //             .then(response => {
+    //                 this.props.setUsersList(response.data.items)
+    //             })
+    //     }
+    // }
 }
+
+
 export default Users
+
---
+++ src/components/Users/UsersFunc.jsx
@@ -0,0 +1,42 @@
+import * as axios from 'axios'
+import css from './Users.module.css'
+import userPhoto from '../../assets/images/avatar4.jpg'
+
+const Users = (props) => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsersList(response.data.items)
+                })
+        }
+    }
+    return (
+        <div className={css.usersInner}>
+            {
+                props.users.map(el => <div key={el.id} className={css.user}>
+                    <div className={css.avaAndButton}>
+                        <div>
+                            <img src={el.photos.small != null ? el.photos.small : userPhoto} />
+                        </div>
+                        <div>
+                            {el.followed
+                                ? <button onClick={() => { props.unfollow(el.id) }}>Отписаться</button>
+                                : <button onClick={() => { props.follow(el.id) }}>Подписаться</button>}
+                        </div>
+                    </div>
+                    <div className={css.main}>
+                        <div>{el.name}</div>
+                        <p>{el.status}</p>
+                    </div>
+                    <div className={css.location}>
+                        <div>{'el.location.city'}</div>
+                        <div>{'el.location.country'}</div>
+                    </div>
+                </div>)}
+            <button className={css.getUsersButton} onClick={getUsers}>Загрузить пользователей</button>
+        </div>
+    )
+}
+export default Users
